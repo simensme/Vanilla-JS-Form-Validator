b3a2e2f9ad6282af8a384224ebc0e8e7d937ab24
Simen Smehaugen <simdevgen@outlook.com>
2024-01-18 09:45:50 +0100

    debugged form-validation

--- script.js
+++ script.js
@@ -7,18 +7,58 @@ const message = document.getElementById('message');
 // Functionality
 
 let isValid = false;
+let passwordsMatch = false;
+
 const validateForm = () => {
     isValid = form.checkValidity();
-    message.textContent = 'Kindly fill out all fields.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
-    
+    if (!isValid) {
+        message.textContent = 'Kindly fill out all fields.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        return;
+    }
+    // Check if passwords match
+    if (password1El.value === password2El.value) {
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    } else {
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return;
+    }
+    // If the form is valid and passwords match
+    if (isValid && passwordsMatch) {
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
+};
+
+const storeFormData = () => {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    };
+    // Manipulate user data
+    console.log(user);
 };
 
 const processFormData = e => {
      e.preventDefault(); /*Have to prevent default as by default a submit will just refresh the page, assuming there is no backend handling it */
     //  Validate Form
     validateForm();
+    // Submit data if the data is valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 };
 
 // Event Listeners
